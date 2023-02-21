10ee7ee489b287eed4936c59d0c971c7997a001a
JUNE <creat.bliv@gmail.com>
2023-02-21 09:11:35 +0900

    feat: 숫자 문자열과 영단어

---
+++ programmers/숫자 문자열과 영단어/index.py
@@ -0,0 +1,6 @@
+# 나의 풀이
+def solution(s):
+    dic = {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four", 5: "five",6: "six", 7: "seven", 8: "eight", 9: "nine"}
+    for x,y in dic.items():
+            s = s.replace(y, str(x))
+    return int(s)
