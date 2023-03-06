79126786948fca5a214352515658bfce85d1ea2e
JUNE <creat.bliv@gmail.com>
2023-03-07 07:21:06 +0900

    feact: 바닐라 스크립트로 리액트처럼 리렌더링 구현하기, 단일 기능 개념 적용해서 응용하기

---
+++ 2023-03-05-01/index.js
@@ -0,0 +1,8 @@
+
+let button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+    // variable in lexical scope get refresh value when click event generated.
+    let input_value = document.getElementById('input').value
+    // and you can use this value for cheking invaliable or form data.
+})
